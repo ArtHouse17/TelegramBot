52fedf4a83b81e8467e193510d30c99ccec2dd8e
ArtHouse17 <artem.demidov.13@mail.ru>
2025-03-05 18:03:43 +0300

    -Исправлена зависимость версии JAVA -Обновлен класс контроллера для изучения KAFKA TODO: Сделать отдельную ветвь для изучения KAFKA

--- backendservice/src/main/java/art/backendservice/controller/MainController.java
+++ backendservice/src/main/java/art/backendservice/controller/MainController.java
@@ -2,12 +2,18 @@ package art.backendservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/example")
 public class MainController {
 
+
+    private final
+    @PostMapping("kafka/send")
+    public String sendMessage(@RequestBody String message) {
+
+        return "Successfully sent message: " + message;
+    }
 }
